b17fd080eb25fc690c5631bd023659398aff3743
Pradyumna Singh Deo <87163520+PradyumnaSinghdeo@users.noreply.github.com>
2024-08-21 20:09:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+document.querySelector('input[type="button"]').addEventListener('click', function() {
+    const colorSelect = document.getElementById('colorSelect');
+    const selectedOption = colorSelect.selectedIndex;
+
+    if (selectedOption !== -1) {
+        colorSelect.remove(selectedOption);
+    }
+});
